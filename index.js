e7e375c7fdfe4aa0e38006350ae1ccaddc73ae21
TTatchell <tim.tatchell24@gmail.com>
2021-03-10 11:46:52 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+let currentUser = "Tim";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let array = [...currentUser];
+
+const shortGreeting = `Welcome, ${array[0]}!`
+
+
+
+console.log(shortGreeting)
